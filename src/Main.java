ae95c26af3a097a68a59b4498526d2d225248281
darryl-ty <98935440+darryl-ty@users.noreply.github.com>
2023-03-10 21:59:18 -0600

    Implemented hayesSort algo and added documentation strings.

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,14 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         int inputNum = 100;
-        ArrayList<Integer> customArray = randomArrayGeneration(inputNum);
+        ArrayList<Integer> customArray = arrayGeneration(inputNum);
 //        System.out.println(selectionSort(customArray));
-        System.out.println(bubbleSort(customArray));
-//        hayesSort(customArray);
+//        System.out.println(bubbleSort(customArray));
+        System.out.println(hayesSort(customArray));
     }
 
-    public static ArrayList<Integer> randomArrayGeneration(int num){
+    public static ArrayList<Integer> arrayGeneration(int num){
+        /** Generates a list of numbers in descending order.*/
         ArrayList<Integer> numList = new ArrayList<>();
         while (num != 0){
             numList.add(num);
@@ -19,6 +20,7 @@ public class Main {
     }
 
     public static ArrayList<Integer> bubbleSort(ArrayList<Integer> inputList){
+        /** Returns a sorted ArrayList of Integers using bubbleSort algorithm. */
         for (int i = 0; i < inputList.size(); i++){
             for (int j = 0; j < inputList.size() - 1 - i; j++){
                 if (inputList.get(j + 1) < inputList.get(j)){
@@ -32,7 +34,21 @@ public class Main {
     }
 
     public static ArrayList<Integer> hayesSort(ArrayList<Integer> inputList){
-        return inputList;
+        /** Returns a sorted ArrayList of integers sorted using professor's "hayesSort" algorithm. */
+        ArrayList<Integer> secondList = new ArrayList<>();
+        int max = inputList.get(0);
+        for (int i = 0; i < inputList.size(); i++){
+            if (inputList.get(i) > max){
+                max = inputList.get(i);
+            }
+        }
+
+        for (int j = 0; j < max; j++){
+            if (inputList.contains(j)){
+                secondList.add(j);
+            }
+        }
+        return secondList;
     }
 
     public static ArrayList<Integer> selectionSort(ArrayList<Integer> inputList){
