45daa820545e387dfb3f3ef75c38642137b02d8b
darryl-ty <98935440+darryl-ty@users.noreply.github.com>
2023-03-10 20:28:08 -0600

    Implements selectionSort method/algorithm.

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,21 @@
 import java.util.ArrayList;
-import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int inputNum = 100;
         ArrayList<Integer> customArray = randomArrayGeneration(inputNum);
-        selectionSort(customArray);
-        bubbleSort(customArray);
-        hayesSort(customArray);
+        System.out.println(selectionSort(customArray));
+//        bubbleSort(customArray);
+//        hayesSort(customArray);
     }
 
     public static ArrayList<Integer> randomArrayGeneration(int num){
         ArrayList<Integer> numList = new ArrayList<>();
+        while (num != 0){
+            numList.add(num);
+            num--;
+        }
         return numList;
     }
 
@@ -25,6 +28,17 @@ public class Main {
     }
 
     public static ArrayList<Integer> selectionSort(ArrayList<Integer> inputList){
+        /** Returns a sorted array through the selectionSort algorithm. */
+        for (int i = 0; i < inputList.size(); i++){
+            int min = i;
+            for (int j = i+1; j < inputList.size(); j++){
+                if (j < min){
+                    min = j;
+                }
+            }
+            inputList.set(i, min);
+            inputList.set(min, i);
+        }
         return inputList;
     }
 }
